51ccbe9ee2d747c35fdcd797ad802a9567ea1bd0
PADIKI DHEERAJ <dheeju@PADIKIs-MacBook-Pro.local>
2019-10-07 08:49:18 +0200

    added latest code

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,17 @@
+var budgetController = (function(){
+
+  //code
+
+})();
+
+var UIController = (function(){
+
+// code
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl){
+// code
+
+})(budgetController, UIController);
+
